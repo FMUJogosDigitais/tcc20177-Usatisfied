18e75e865374d27abb83db893b8bc2100cd23d98
renatoinnocenti <renato.innocenti@gmai.com>
2017-11-12 13:01:13 -0200

    Actualzation fixed

---
+++ Usatisfied Digital/Assets/Scripts/AudioScripts/MusicManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicManager : MonoBehaviour {
+
+	[Header("Musicas de BackGround")]
+	[SerializeField] AudioClip[] bgMusic;
+
+	private AudioSource myAudioSource;
+
+	// Use this for initialization
+	void Start () {
+		myAudioSource = GetComponent<AudioSource> ();
+		BgMusic ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void BgMusic ()
+	{
+		int xMusic = Random.Range (0, bgMusic.Length);
+		myAudioSource.clip = bgMusic [xMusic];
+		myAudioSource.Play ();
+	}
+
+	public void StopBGMusic()
+	{
+		myAudioSource.Stop ();
+	}
+}
---
+++ Usatisfied Digital/Assets/Scripts/AudioScripts/PlaySFX.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaySFX : MonoBehaviour {
+
+	private AudioSource myAudioSource;
+
+	// Use this for initialization
+	void Start () {
+
+		myAudioSource = GetComponent<AudioSource> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void SatisfactionSFX(){
+		myAudioSource.Play ();
+	}
+
+
+}
